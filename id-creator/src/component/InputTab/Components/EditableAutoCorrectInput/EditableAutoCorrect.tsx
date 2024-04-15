300e512e55b5d7b9310f51879fb0916d03c46239
ggbb0711 <ggbotcute07@gmail.com>
2024-04-15 12:29:20 +0700

    fix custom effect

--- id-creator/src/component/InputTab/Components/EditableAutoCorrectInput/EditableAutoCorrect.tsx
+++ id-creator/src/component/InputTab/Components/EditableAutoCorrectInput/EditableAutoCorrect.tsx
@@ -8,21 +8,21 @@ import ContentEditable from "react-contenteditable";
 
 export default function EditableAutoCorrect({inputId,content,matchList,changeHandler}:{inputId:string,content:string,changeHandler:(e:React.ChangeEvent<HTMLInputElement>)=>void,matchList:{[key:string]:string}}):ReactElement{
     const contentEditableRef=useRef(null)
-    const {statusEffect} = useStatusEffectContext()
 
     useEffect(()=>{
         if(contentEditableRef){
             const editable= new ContenteditableEditor(contentEditableRef.current)
+
             const textComplete= new Textcomplete(editable,[{
                 match: /\[([\-+\w]*)$/,
                 search(term, callback, match) {
-                    callback(Object.keys(statusEffect).map(key=>[key,statusEffect[key]]).filter(value=>value[0].match(term)))
+                    callback(Object.keys(matchList).map(key=>[key,matchList[key]]).filter(value=>value[0].match(term)))
                 },
                 template: ([keyWord, html]) =>html,
                 replace: (result): string => `[${result[0]}] `
             }],{dropdown:{maxCount:5,item:{className: "textcomplete-item",activeClassName: "textcomplete-item active",}}})
         }
-    },[])
+    },[JSON.stringify(matchList)])
     
     return  <ContentEditable id={inputId}
     className="input skill-effect-input"
