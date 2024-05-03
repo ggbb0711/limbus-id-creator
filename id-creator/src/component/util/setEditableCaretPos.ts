5afa15914c14df17546bac3cef3fb19fbab4cc34
ggbb0711 <ggbotcute07@gmail.com>
2024-05-03 21:06:48 +0700

    fix the bug where the caret keep randomly jump to a location

--- id-creator/src/component/util/setEditableCaretPos.ts
+++ id-creator/src/component/util/setEditableCaretPos.ts
@@ -1,26 +1,31 @@
 const setEditableCaretPos = (contentEle:HTMLElement,targetPosition:number) => {
+    if(targetPosition===-1) return -1
+    let finished = false
     contentEle.childNodes.forEach(node=>{
-        if(node.nodeType===3){//Text node
-            if(node.textContent.length>=targetPosition){
-                const range = document.createRange()
-                const sel = window.getSelection()
-                range.setStart(node,targetPosition)
-                range.collapse(true)
-                sel.removeAllRanges()
-                sel.addRange(range)
-                return -1//Done
-            }else{
-                targetPosition-=node.textContent.length
+        if(!finished){
+            if(node.nodeType===3){//Text node
+                if(node.textContent.length>=targetPosition){
+                    const range = document.createRange()
+                    const sel = window.getSelection()
+                    range.setStart(node,targetPosition)
+                    range.collapse(true)
+                    sel.removeAllRanges()
+                    sel.addRange(range)
+                    finished = true//Done
+                }else{
+                    targetPosition-=node.textContent.length
+                }
+            }
+            else{
+                targetPosition = setEditableCaretPos((node as HTMLElement),targetPosition)
+                if(targetPosition===-1){
+                    finished = true
+                }
             }
         }
-        else{
-            targetPosition = setEditableCaretPos((node as HTMLElement),targetPosition)
-            if(targetPosition===-1){
-                return -1
-            }
-        }
+        
     })
-    return targetPosition//Done
+    return (finished)?-1:targetPosition//Done
 }
 
 export default setEditableCaretPos
