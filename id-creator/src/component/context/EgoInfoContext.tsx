4a421faadd7511469b4f9c39931d5ab0bc3641de
ggbb0711 <ggbotcute07@gmail.com>
2024-05-16 16:17:31 +0700

    Add new id-ego context

--- id-creator/src/component/context/EgoInfoContext.tsx
+++ id-creator/src/component/context/EgoInfoContext.tsx
@@ -1,47 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { ReactElement, createContext, useContext, useState } from "react";
-import { OffenseSkill } from 'Interfaces/OffenseSkill/IOffenseSkill';
-import { PassiveSkill } from 'Interfaces/PassiveSkill/IPassiveSkill';
 import { IEgoInfo } from 'Interfaces/IEgoInfo';
 
 const egoInfo = createContext(null)
 
-const EgoInfoProvider: React.FC<{children:ReactElement}>=({children})=>{
-    const [EgoInfoValue,setEgoInfoValue]=useState<IEgoInfo>(
-        {
-            title:"",
-            name:"",
-            sanityCost:0,
-            splashArt:"",
-            splashArtScale:1,
-            splashArtTranslation:{
-                x:0,
-                y:0,
-            },
-            sinResistant:{
-                Wrath_resistant:1,
-                Lust_resistant:1,
-                Sloth_resistant:1,
-                Gluttony_resistant:1,
-                Gloom_resistant:1,
-                Pride_resistant:1,
-                Envy_resistant:1,
-            },
-            sinCost:{
-                Wrath_cost:0,
-                Lust_cost:0,
-                Sloth_cost:0,
-                Gluttony_cost:0,
-                Gloom_cost:0,
-                Pride_cost:0,
-                Envy_cost:0,  
-            },
-            sinnerColor:"var(--Yi-Sang-color)",
-            sinnerIcon:"Images/sinner-icon/Yi_Sang_Icon.png",
-            egoLevel:"ZAYIN",
-            skillDetails:[new OffenseSkill("Awakening","Wrath",1,"AWAKENING"),new OffenseSkill("Corrision","Wrath",1,"CORRISION"),new PassiveSkill("Passive","PASSIVE")]
-        }
-    )
+const EgoInfoProvider: React.FC<{children:ReactElement,EgoInfoValue:IEgoInfo,setEgoInfoValue:React.Dispatch<React.SetStateAction<IEgoInfo>>}>=({children,EgoInfoValue,setEgoInfoValue})=>{
+
 
     return <egoInfo.Provider value={{EgoInfoValue,setEgoInfoValue}}>
             {children}
--- id-creator/src/component/context/IdInfoContext.tsx
+++ id-creator/src/component/context/IdInfoContext.tsx
@@ -1,39 +1,12 @@
 import React, { useEffect } from 'react'
 import { ReactElement, createContext, useContext, useState } from "react";
 import { IIdInfo } from "../../Interfaces/IIdInfo";
-import { OffenseSkill } from 'Interfaces/OffenseSkill/IOffenseSkill';
-import { DefenseSkill } from 'Interfaces/DefenseSkill/IDefenseSkill';
-import { PassiveSkill } from 'Interfaces/PassiveSkill/IPassiveSkill';
 
 const idInfo = createContext(null)
 
-const IdInfoProvider: React.FC<{children:ReactElement}>=({children})=>{
-    const [idInfoValue,setIdInfoValue]=useState<IIdInfo>(
-        {
-            title:"",
-            name:"",
-            splashArt:"",
-            splashArtScale:1,
-            splashArtTranslation:{
-                x:0,
-                y:0,
-            },
-            HP:0,
-            minSpeed:0,
-            maxSpeed:0,
-            staggerResist:"",
-            defenseLevel:0,
-            slashResistant:1,
-            pierceResistant:1,
-            bluntResistant:1,
-            sinnerColor:"var(--Yi-Sang-color)",
-            sinnerIcon:"Images/sinner-icon/Yi_Sang_Icon.png",
-            rarity:"Images/rarity/IDNumber1.png",
-            skillDetails:[new OffenseSkill("Skill 1","Wrath",3,"SKILL 1"),new OffenseSkill("Skill 2","Gluttony",2,"SKILL 2"),new OffenseSkill("Skill 3","Pride",1,"SKILL 3"),new DefenseSkill("Defense"),new PassiveSkill("Passive"), new PassiveSkill("Support","SUPPORT")],
-        }
-    )
+const IdInfoProvider: React.FC<{children:ReactElement,idInfoProp:IIdInfo,setIdInfoValue: React.Dispatch<React.SetStateAction<IIdInfo>>}>=({children,idInfoProp,setIdInfoValue})=>{
 
-    return <idInfo.Provider value={{idInfoValue,setIdInfoValue}}>
+    return <idInfo.Provider value={{idInfoValue:idInfoProp,setIdInfoValue}}>
             {children}
         </idInfo.Provider>;
 }
