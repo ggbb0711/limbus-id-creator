33ece10c7098111ae0ccf75c605bf1ce944f8be8
ggbb0711 <ggbotcute07@gmail.com>
2024-08-18 11:37:21 +0700

    Fix local save

--- id-creator/src/component/SaveMenu/SaveLocalMenu/SaveLocalMenu.tsx
+++ id-creator/src/component/SaveMenu/SaveLocalMenu/SaveLocalMenu.tsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../SaveMenu.css"
 import { ISaveFile, SaveFile } from "Interfaces/ISaveFile";
 import MainButton from "utils/MainButton/MainButton";
 import { IIdInfo } from "Interfaces/IIdInfo";
 import { IEgoInfo } from "Interfaces/IEgoInfo";
 import useSaveLocal from "utils/Hooks/useSaveLocal";
+import uuid from "react-uuid";
 
 
 
 const SaveLocalMenu=({localSaveName,saveObjInfoValue,loadObjInfoValueCb,isActive,setIsActive}:{localSaveName:string, saveObjInfoValue:ISaveFile<IIdInfo|IEgoInfo>, loadObjInfoValueCb:React.Dispatch<React.SetStateAction<IIdInfo|IEgoInfo>>,isActive:boolean,setIsActive:(a:boolean)=>void})=>{
     const {saveData,changeSaveName,deleteSave,createSave,loadSave,overwriteSave} = useSaveLocal<IIdInfo|IEgoInfo>(localSaveName)
 
+    //Some of the save can have the same id
+    //This is to create new save for some 
+    //of the save with same id
+    useEffect(()=>{
+        saveData.map(save=>{
+            if(saveData.some(s=>s.id===save.id)) save.id = uuid()
+            return save
+        })
+    },[localSaveName])
 
     return<>
             <div className="save-menu-list local">
                 {saveData.length>0?<>
                     {saveData.map((data,i)=>
-                        <div className={`save-tab center-element`} key={data.saveTime}>
+                        <div className={`save-tab center-element`} key={data.id}>
                             {data.previewImg?<img className="save-preview-img" src={data.previewImg} alt="preview-save" />:<></>}
                             <div>
                                 <p className="created-time">Created on: {data.saveTime}</p>
@@ -49,6 +59,7 @@ const SaveLocalMenu=({localSaveName,saveObjInfoValue,loadObjInfoValueCb,isActive
                     const maxLenStr = process.env.REACT_APP_LOCAL_SAVE_MAX_LEN
                     const maxLen = Number(maxLenStr)??10
                     if(saveData.length<maxLen){
+                        saveObjInfoValue.id = uuid()
                         saveObjInfoValue.saveTime = new Date().toLocaleString()
                         createSave(saveObjInfoValue)
                     }
