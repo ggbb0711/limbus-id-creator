1291ec33b398a6e27d899a89c6581c5b5c62e9ce
bb <99488111+ggbb0711@users.noreply.github.com>
2026-03-01 08:48:34 +0700

    Update in-feed ads

--- id-creator/src/Components/InFeedAd/InFeedAd.tsx
+++ id-creator/src/Components/InFeedAd/InFeedAd.tsx
@@ -23,7 +23,7 @@ export default function InFeedAd() {
         <ins
             ref={insRef}
             className="adsbygoogle"
-            style={{ display: "block", textAlign: "center" }}
+            style={{ display: "block", textAlign: "center", width: "100%" }}
             data-ad-layout="in-article"
             data-ad-format="fluid"
             data-ad-client="ca-pub-2161757435040376"
